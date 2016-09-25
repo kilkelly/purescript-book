3d2cd46a219e9632922736b289e01b7f636caea1
Frank Kilkelly <frank.kilkelly@gmail.com>
2016-09-25 17:26:40 +0100

    Fix calculations of top and bottom shape bounds
    I think the calculations of the top and bottom of shape bounds was mistakenly reversed. Also the union and intersect functions were affected. Hope this is correct!

--- chapter5/src/Data/Picture.purs
+++ chapter5/src/Data/Picture.purs
@@ -53,21 +53,21 @@ showBounds (Bounds b) =
 
 shapeBounds :: Shape -> Bounds
 shapeBounds (Circle (Point { x, y }) r) = Bounds
-  { top:    y - r
+  { top:    y + r
   , left:   x - r
-  , bottom: y + r
+  , bottom: y - r
   , right:  x + r
   }
 shapeBounds (Rectangle (Point { x, y }) w h) = Bounds
-  { top:    y - h / 2.0
+  { top:    y + h / 2.0
   , left:   x - w / 2.0
-  , bottom: y + h / 2.0
+  , bottom: y - h / 2.0
   , right:  x + w / 2.0
   }
 shapeBounds (Line (Point p1) (Point p2)) = Bounds
-  { top:    Math.min p1.y p2.y
+  { top:    Math.max p1.y p2.y
   , left:   Math.min p1.x p2.x
-  , bottom: Math.max p1.y p2.y
+  , bottom: Math.min p1.y p2.y
   , right:  Math.max p1.x p2.x
   }
 shapeBounds (Text (Point { x, y }) _) = Bounds
@@ -79,9 +79,9 @@ shapeBounds (Text (Point { x, y }) _) = Bounds
 
 union :: Bounds -> Bounds -> Bounds
 union (Bounds b1) (Bounds b2) = Bounds
-  { top:    Math.min b1.top    b2.top
+  { top:    Math.max b1.top    b2.top
   , left:   Math.min b1.left   b2.left
-  , bottom: Math.max b1.bottom b2.bottom
+  , bottom: Math.min b1.bottom b2.bottom
   , right:  Math.max b1.right  b2.right
   }
 
@@ -89,9 +89,9 @@ infixl 4 union as \/
 
 intersect :: Bounds -> Bounds -> Bounds
 intersect (Bounds b1) (Bounds b2) = Bounds
-  { top:    Math.max b1.top    b2.top
+  { top:    Math.min b1.top    b2.top
   , left:   Math.max b1.left   b2.left
-  , bottom: Math.min b1.bottom b2.bottom
+  , bottom: Math.max b1.bottom b2.bottom
   , right:  Math.min b1.right  b2.right
   }
 
